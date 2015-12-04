a90878927a3254518638893ba88fd9e0d057ff5c
Ganesh9531 <Ganesh9531@IND-PNE3LW70125.mastek.com>
2015-12-04 16:00:24 +0530

    added a new file godhatesme and edited Gopal.java

--- demoWebApp-1/src/com/gj/Gopal.java
+++ demoWebApp-1/src/com/gj/Gopal.java
@@ -33,7 +33,7 @@ public class Gopal extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("added log");
 	}
 
 }
